cc9344aa47fce62b12434120d59815dc996f81ec
Erik Bjäreholt <erik@bjareho.lt>
2017-08-18 09:13:49 +0200

    Changed to wip tests not using mocha

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 aw-server --testing --storage=memory &> /dev/null &
 AWPID=$!
-sleep 5; mocha
+
+sleep 5  # Give some time to start
+
+node test.js
+# TODO Switch back to using mocha
+# mocha
+
 kill $AWPID
