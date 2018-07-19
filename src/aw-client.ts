fb13fe2fca74cebed2304e71521329bba149595d
mathieudutour <mathieu@dutour.me>
2018-07-19 06:50:20 -0600

    use default parameter instead of falsy check

--- src/aw-client.ts
+++ src/aw-client.ts
@@ -54,10 +54,7 @@ export class AWClient {
         }
     } = {};
 
-    constructor(clientname: string, options?: {testing?: boolean, baseURL?: string}) {
-        if (!options) {
-          options = {}
-        }
+    constructor(clientname: string, options?: {testing?: boolean, baseURL?: string} = {}) {
         this.clientname = clientname;
         this.testing = options.testing || false;
         if (typeof options.baseURL === 'undefined') {
